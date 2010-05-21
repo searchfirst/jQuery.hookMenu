c29954fe163a786197b633a6ea4f12ffb5a501c4
Paul <paul@shunuk.co.uk>
2010-05-21 04:34:15 -0700

    Oops.
    I had a better revised version on Dux instead of Moonlight. Merged the two manually and this is the improved product

--- jquery.hookMenu.js
+++ jquery.hookMenu.js
@@ -1,33 +1,40 @@
 jQuery.fn.hookMenu = function(settings) {
 	settings = jQuery.extend({
 		'growUp':false,
-		'fixed':false		
+		'position':'absolute'		
 	},settings);
 	var hook_menu = new Array();
-	var hook_parent = new Array();
 	var hook_menu_x = new Array();
 	var hook_menu_xy = new Array();
-	var hook_menu_container = new Array();
+	var hook_menu_xc = new Array();
+	var current_hook = new Array();
 	this.each(function(i) {
-		hook_parent[i] = jQuery(this);
+		current_hook[i] = jQuery(this);
 		hook_menu[i] = jQuery(this).next('ul.hook_menu');
 		if(hook_menu[i].length) {
-			jQuery(this).append('<span class="hook_menu_x">Expand</span>');
-			hook_menu_x[i] = jQuery(this).children('span.hook_menu_x').eq(0);
+			current_hook[i].append('<span class="hook_menu_x">Expand</span>');
+			hook_menu_x[i] = current_hook[i].children('span.hook_menu_x').eq(0);
+			current_hook[i].wrapInner('<span class="hook_menu_xc"/>');
+			hook_menu_xc[i] = current_hook[i].children().eq(0);
 			hook_menu_xy[i] = {
-				'width': Math.max((hook_menu_x[i].offset().left + hook_menu_x[i].outerWidth() - jQuery(this).offset().left) - (hook_menu[i].outerWidth() - hook_menu[i].width()),'125'),
-				'top': jQuery(this).offset().top + jQuery(this).height(),
-				'left': jQuery(this).offset().left
+				'width': Math.max(hook_menu_xc[i].outerWidth(),175),
+				'top': current_hook[i].offset().top + current_hook[i].height(),
+				'left': current_hook[i].offset().left
 			};
+			hook_menu[i].recalcCoords = function() {
+				hook_menu[i].css({
+					'left': current_hook[i].offset().left,
+					'top': hook_menu_xc[i].offset().top + hook_menu_xc[i].height(),
+					'width': Math.max(hook_menu_xc[i].outerWidth(),175)
+				});
+			}
 			hook_menu[i].css({
-				'position': 'absolute',
+				'position': settings.position,
 				'display': 'none',
 				'left': hook_menu_xy[i].left,
 				'width': hook_menu_xy[i].width,
 				'top': hook_menu_xy[i].top
 			});
-			jQuery(this).wrapInner('<span class="hook_menu_xc"/>');
-			hook_menu_container[i] = jQuery(this).children('span.hook_menu_xc').eq(0);
 			hook_menu_x[i].bind({
 				mouseenter: function(e) {
 					jQuery(this).parent().toggleClass('hook_highlight');
@@ -36,21 +43,8 @@ jQuery.fn.hookMenu = function(settings) {
 					jQuery(this).parent().toggleClass('hook_highlight');
 				},
 				click: function(e) {
-
-					hook_menu_xy[i] = {
-						'width': Math.max((hook_menu_x[i].offset().left + hook_menu_x[i].outerWidth() - jQuery(this).offset().left) - (hook_menu[i].outerWidth() - hook_menu[i].width()),'125'),
-						'top': hook_menu_container[i].offset().top + hook_menu_container[i].height(),
-						'left': hook_parent[i].offset().left
-					};
-					hook_menu[i].css({
-						'position': 'absolute',
-						'display': 'none',
-						'left': hook_menu_xy[i].left,
-						'width': hook_menu_xy[i].width,
-						'top': hook_menu_xy[i].top
-					});
-
-					hook_menu[i].fadeIn('slow');
+					hook_menu[i].recalcCoords();
+					hook_menu[i].show();
 				}
 			});
 			hook_menu[i].bind({
